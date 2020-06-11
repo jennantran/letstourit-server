7fefaa8f8eec1cab9032b9b0d3f7b2e5fcbc748f
Jennifer Tran <jennantran@gmail.com>
2020-06-11 14:40:26 -0700

    ready for heroku

--- src/app.js
+++ src/app.js
@@ -11,23 +11,21 @@ app.get('/api/', (req, res) => {
     res.send('Hello, world!')
      })
 
-     const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
-app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use(function errorHandler(error, req, res, next) {
-   let response
-   if (NODE_ENV === 'production') {
-         response = { error: { message: 'server error' } }
-       } else {
-         console.error(error)
-         response = { message: error.message, error }
-       }
-       res.status(500).json(response)
-     })
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
     
 module.exports = app
