994156ed631e5d854ee7627c23c1a0187eaad863
Jennifer Tran <jennantran@gmail.com>
2020-06-17 23:11:14 -0700

    modified middleware

--- src/middleware/jwt-auth.js
+++ src/middleware/jwt-auth.js
@@ -2,23 +2,19 @@ const AuthService = require('../auth/auth-service')
 
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
-  console.log('in jwt');
   let bearerToken
   if (!authToken.toLowerCase().startsWith('bearer ')) {
     return res.status(401).json({ error: 'Missing bearer token' })
   } else {
     bearerToken = authToken.slice(7, authToken.length)
   }
-  console.log(bearerToken);
   try {
     const payload = AuthService.verifyJwt(bearerToken)
-    console.log(payload);
     AuthService.getUserWithUserName(
       req.app.get('db'),
       payload.sub,
     )
       .then(user => {
-        console.log(user);
         if (!user)
           return res.status(401).json({ error: 'Unauthorized request' })
 
@@ -26,7 +22,6 @@ function requireAuth(req, res, next) {
         next()
       })
       .catch(err => {
-        console.error(err)
         next(err)
       })
   } catch(error) {
