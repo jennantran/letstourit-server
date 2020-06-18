5d7c4ca0b44fbedcf6f7779ed398cc628a305c72
Jennifer Tran <jennantran@gmail.com>
2020-06-17 21:13:44 -0700

    complete favorites-service test file

--- test/favorites-service.spec.js
+++ test/favorites-service.spec.js
@@ -1,16 +1,19 @@
 const FavoritesService = require('../src/favorites/favorites-service');
 const knex = require('knex');
+const app = require('../src/app');
+const helpers = require('./test-helpers');
+const chai = require('chai');
 
   describe(`Favorites service object`, function() {
-       let db
-          let testUsers = [
-         {
+        let db
+        let testUsers = [
+            {
               id: 1,
               username: 'jennantran8',
               password:'$2a$12$5IzM0iAWkSzN.tuzA7gOfuAaUshNP6M2Qa8yZitgeBEItE0IS1J3m',
-             },
-           ]
-         let testFavorites =[
+            },
+        ]
+        let testFavorites =[
              {
                 place_id:'ChIJD9j4j9R_j4AR4Ux0kcDrJq4',
                 name:'Academy of Art University',
@@ -18,16 +21,19 @@ const knex = require('knex');
                 address:'79 New Montgomery St, San Francisco, CA 94105, USA',
                 user_id: 1,
              },
-         ]
-       before(() => {
-         db = knex({
-           client: 'pg',
-           connection: process.env.TEST_DB_URL,
-         })
-       })
-       
-       before('clean the tables before ', () => db.raw('TRUNCATE TABLE tour_users, save_tour_favorites RESTART IDENTITY CASCADE'));
+        ]
+        before(() => {
+            db = knex({
+                client: 'pg',
+                connection: process.env.TEST_DB_URL,
+            })
+            app.set('db',db)
+        })
+        before('clean the tables before ', () => db.raw('TRUNCATE TABLE tour_users, save_tour_favorites RESTART IDENTITY CASCADE'));
+        afterEach('clean the tables afterEach', () =>  db.raw('TRUNCATE TABLE tour_users, save_tour_favorites RESTART IDENTITY CASCADE')); 
+        after('disconnect from db',() => db.destroy()) 
 
+    context(`Given 'tour_favorites' has data`, () => {
         before(() => {
             return db
                 .into('tour_users')
@@ -38,28 +44,46 @@ const knex = require('knex');
                 .into('save_tour_favorites')
                 .insert(testFavorites)
             })
-        after(() => db.destroy())
-
-describe(`getAllFavorites()`, () => {
-     it(`resolves all favorites from 'save_tour_favorites' table`, () => {
+    
+       it(`getAllFavorites() resolves all favorites from 'tour_favorites' table`, () => {
        // test that FavoritesService.getFavorites gets data from table
+   
         return FavoritesService.getAllFavorites(db)
          .then(actual => {
          expect(actual).to.eql(testFavorites)
       })
      })   
+     it(`deleteFavorites() remove a favorite by placeId`, () => {
+        return FavoritesService.deleteFavorite(db, 'ChIJD9j4j9R_j4AR4Ux0kcDrJq4')  
    })
-   it(`it insert favorites into save_tour_favorites table`, () => {
-    return FavoritesService.insertFavorite(knex, newFavorite)
-        .then(actual => {
-            expect(actual).to.eql([{
-                place_id:'ChIJawCN2HyAhYARNagMvTWlbKw',
-                name:'sweetgreen',
-                rating: '4',
-                address:'171 2nd St San Francisco, CA 94105',
-                user_id: 1,
-            }]);
+ 
+})
+    context(`Given 'tour_favorites' has no data`, () => {
+        beforeEach('get user ids', () => {
+            return db
+                .into('tour_users')
+                .insert(testUsers)
+                .returning('id')
+                .then((res) => {
+                    userId1 = res[0];
+                });
         });
-});
 
+       it(`getAllFavorites() resolves an empty array`, () => {
+         return FavoritesService.getAllFavorites(db)
+           .then(actual => {
+             expect(actual).to.eql([])
+           })
+       })
+       it(`insertFavorite() inserts a new favorite and resolves the new favorite with an 'id'`, () => {
+          const newFavorite = {
+            place_id: 'ChIJawCN2HyAhYARNagMvTWlbKw',
+            name: 'sweetgreen',
+            rating: '4',
+            address: '171 2nd St San Francisco, CA 94105',
+            user_id: userId1,
+          }
+          return FavoritesService.insertFavorite(db, newFavorite)
+       })
+    })
   })
