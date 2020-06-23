3d383201508d5ec3c35d9503897d21786b598bba
Jennifer Tran <jennantran@gmail.com>
2020-06-23 10:08:54 -0700

    update user service and user router to fix sign up

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,4 +1,18 @@
+const bcrypt = require('bcryptjs')
+const xss = require('xss')
+
+const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 const UsersService = {
+    getAllUsers(knex){
+        return knex.select('*')
+            .from('tour_users')
+    },
+    hasUserWithUserName(db, username) {
+        return db('tour_users')
+          .where({ username })
+          .first()
+          .then(user => !!user)
+      },
     getById(knex,id){
         return knex
             .from('tour_users')
@@ -6,6 +20,29 @@ const UsersService = {
             .where('id',id)
             .first()
     },
+    insertUser(knex, newUser) {
+        return knex
+            .insert({
+                username: `${newUser.username}`,
+                password:`${newUser.password}`
+            })
+            .into('tour_users')
+            .returning('*')
+            .then(rows => {
+                return rows[0]
+            })
+    },
+
+      hashPassword(password) {
+        return bcrypt.hash(password, 12)
+      },
+      serializeUser(user) {
+        return {
+          id: user.id,
+          username: xss(user.username),
+
+        }
+      },
 }
 
 module.exports = UsersService;
