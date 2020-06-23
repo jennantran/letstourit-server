a855c1ea017bd313a75dcc648e5773f2109fe599
Jennifer Tran <jennantran@gmail.com>
2020-06-23 14:29:50 -0700

    feature change

--- src/app.js
+++ src/app.js
@@ -18,17 +18,6 @@ app.use(helmet())
 
 app.use(cors());
 
-// app.use(function validateBearerToken(req, res, next) {
-//     const apiToken = process.env.API_TOKEN
-//     const authToken = req.get('Authorization')
-  
-//     if (!authToken || authToken.split(' ')[1] !== apiToken) {
-//       return res.status(401).json({ error: 'Unauthorized request' })
-//     }
-//     // move to the next middleware
-//     next()
-//   })
-
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
